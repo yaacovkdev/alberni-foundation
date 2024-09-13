26b6ad604e830fc5ed4b7f4f323251479b500eac
jacobkochatkov <jacob.kochatkov@gmail.com>
2024-09-13 15:14:07 -0700

    give back ribbon style

--- src/components/home/giveback_ribbon/GivebackRibbon.tsx
+++ src/components/home/giveback_ribbon/GivebackRibbon.tsx
@@ -6,21 +6,27 @@ export default function GivebackRibbon() {
         <div className="giveback-ribbon">
             <div className="giveback-ribbon__blocks">
                 <div className="dollarsign-block">
-                    <h2>$27K</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <div className="dollarsign-block__content">
+                        <h2 className="header-font-size">$27K</h2>
+                        <p className="sans-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                </div>
+                <div className="dollarsign-block dollarsign-block--middle">
+                    <div className="dollarsign-block__content">
+                        <h2 className="header-font-size">$27K</h2>
+                        <p className="sans-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
                 </div>
                 <div className="dollarsign-block">
-                    <h2>$27K</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-                <div className="dollarsign-block dollarsign-block--last">
-                    <h2>$27K</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <div className="dollarsign-block__content">
+                        <h2 className="header-font-size">$27K</h2>
+                        <p className="sans-font">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
                 </div>
             </div>
             <div className="giveback-ribbon__footer">
                 <p>Want to give back to the community?</p>
-                <Link className="support-btn" href="/donations">Support Us</Link>
+                <Link className="giveback-ribbon__support-btn" href="/donations">Support Us</Link>
             </div>
 
 
