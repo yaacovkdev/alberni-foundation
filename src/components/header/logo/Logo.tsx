e1d26a23772df13e3c983af5c5ed278dd26816f2
jacobkochatkov <jacob.kochatkov@gmail.com>
2024-09-12 18:05:24 -0700

    design consistency; home foundation and hero component

---
+++ src/components/header/logo/Logo.tsx
@@ -0,0 +1,9 @@
+import "./Logo.css";
+
+export default function Logo() {
+    return (
+        <div className="logo">
+
+        </div>
+    );
+}
---
+++ src/components/home/hero/Hero.tsx
@@ -0,0 +1,9 @@
+import "./Hero.css";
+
+export default function Hero() {
+    return (
+      <div className="hero hero-image">
+          <h1 className="hero__text title-font-size">Home Hero Title</h1>
+      </div>
+    );
+}
