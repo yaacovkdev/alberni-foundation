3843a1eee91b96fa474c65d560822580ff3fd785
jacobkochatkov <jacob.kochatkov@gmail.com>
2024-09-14 21:49:53 -0700

    donations

--- src/app/giving/page.tsx
+++ src/app/giving/page.tsx
@@ -1,9 +1,11 @@
 import ModelHero from "@/components/giving_model/model_hero/ModelHeader";
+import ModelReasons from "@/components/giving_model/model_reasons/ModelReasons";
 
 export default function page() {
   return (
     <article className="giving">
       <ModelHero />
+      <ModelReasons />
     </article>
   )
 }
